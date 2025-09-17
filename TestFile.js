c6ca900841f0b8c81c024b48426ba8b310f2ae53
pantzlaffw1606 <pantzlaffw1606@my.uwstout.edu>
2025-09-17 16:02:53 -0500

    Added My name

--- TestFile.js
+++ TestFile.js
@@ -1,3 +1,4 @@
 //Please copy this command and add your name to confirm your 
 // connectedness to GitHub repo.
 console.log("Hello,World! I am Connor.");
+console.log("Hello,World! I am William.");
