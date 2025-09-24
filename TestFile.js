a2085f6638e330fe6aac929eb60206ee34e22731
larsonc3385 <larsonc3385@my.uwstout.edu>
2025-09-24 14:54:29 -0500

    Edit to Melissa test

--- TestFile.js
+++ TestFile.js
@@ -4,8 +4,4 @@ console.log("Hello,World! I am Connor.");
 console.log("Hello,World! I am William.");
 console.log("Hello,World! I am Damien >:).");
 console.log("Hello,World! I am Carter.");
-<<<<<<< HEAD
 console.log("Hello,World! I am Melissa.");
-=======
-console.log("Hello,World! I am Melissa.-_-");
->>>>>>> 1216eb5f8ca602494cf7c0ef4e804e0e5bb399e9
