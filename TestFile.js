5ebf48cd5a00723b7df3d03e38d1b269b548fb84
Seth Berrier <berriers@uwstout.edu>
2025-10-08 16:02:25 -0500

    Clearing out ignored files

--- TestFile.js
+++
@@ -1,7 +0,0 @@
-//Please copy this command and add your name to confirm your 
-// connectedness to GitHub repo.
-console.log("Hello,World! I am Connor.");
-console.log("Hello,World! I am William.");
-console.log("Hello,World! I am Damien >:).");
-console.log("Hello,World! I am Carter.");
-console.log("Hello,World! I am Melissa.");
