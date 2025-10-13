7ebcf25304485d3f4bb102562ec62669900fddd1
Seth Berrier <berriers@uwstout.edu>
2025-10-13 16:31:33 -0500

    Added HTTPS certificate and config for backend.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,19 @@
-import dotenv from 'dotenv'
+import fs from 'node:fs'
+import path from 'node:path'
+import https from 'node:https'
+
 import Express from 'express'
+import dotenv from 'dotenv'
 import morgan from 'morgan'
 
 // Import the individual routers
 import UserRoutes from './routes/users.js'
 import DataRoutes from './routes/data.js'
+
+// Read in development certificate info
+const privateKey = fs.readFileSync(path.join('devCert', 'privateDevKey.pem'), 'utf8');
+const certificate = fs.readFileSync(path.join('devCert', 'devCert.pem'), 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 // Configure environment variables
 dotenv.config()
@@ -13,6 +22,7 @@ const LISTEN_PORT = process.env.LISTEN_PORT ?? 3000
 // Creates the express server app
 const app = new Express()
 
+
 // Attach universal app filters
 app.use(morgan('dev'))
 
@@ -20,7 +30,8 @@ app.use(morgan('dev'))
 app.use('/user', UserRoutes)
 app.use('/data', DataRoutes)
 
-// Listen
-app.listen(LISTEN_PORT, () => {
-    console.log(`Server listening on http://127.0.0.1:${LISTEN_PORT}`)
+// Setup secure server and listen
+const httpsServer = https.createServer(credentials, app)
+httpsServer.listen(LISTEN_PORT, () => {
+    console.log(`Server listening on https://127.0.0.1:${LISTEN_PORT}`)
 })
