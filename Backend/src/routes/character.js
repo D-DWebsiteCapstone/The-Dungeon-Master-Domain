0bb916e35752c2567d9e0898c496f7173880aba6
DamienEhlinger <157526219+DamienEhlinger@users.noreply.github.com>
2025-10-27 15:46:49 -0500

    Add character retrieval routes and implement character creation endpoint. Will need to see if this works.

--- Backend/src/routes/character.js
+++ Backend/src/routes/character.js
@@ -11,6 +11,7 @@ const router = new express.Router()
 
 router.use(express.json())
 
+// Route to get character by ID
 router.get('/character/:id', async (req, res) => {
     const characterId = req.params.id
     const character = await getCharacter(characterId)
@@ -21,10 +22,50 @@ router.get('/character/:id', async (req, res) => {
     }
 })
 
+// Route to get character by Name
+router.get('/character/name/:name', async (req, res) => {
+    const characterName = req.params.name
+    const character = await getCharacterByName(characterName)   
+    if (!character) {
+        res.status(404).json({ valid: false, message: 'Character name not found or something went OOF' })
+    } else {
+        res.json({ valid: true, character })
+    }  
+})
 
+// Route to get character by Image
+router.get('/character/image/:image', async (req, res) => {
+    const characterImage = req.params.image
+    const character = await getCharacterByImage(characterImage)
+    if (!character) {
+        res.status(404).json({ valid: false, message: 'Character image not found or something went OOF' })
+    } else {
+        res.json({ valid: true, character })
+    }
+})
 
+// Route to get character by Backstory
+router.get('/character/backstory/:backstory', async (req, res) => {
+    const characterBackstory = req.params.backstory
+    const character = await getCharacterByBackstory(characterBackstory)
+    if (!character) {
+        res.status(404).json({ valid: false, message: 'Character backstory not found or something went OOF' })
+    } else {
+        res.json({ valid: true, character })
+    }
+})
 
+//This part will be for posting new characters to the database
 
+router.post('/character', async (req, res) => {
+    const { id, name, image, backstory } = req.body
+    const newCharacter = await createCharacter({ id, name, image, backstory })
+    if (!newCharacter) {
+        res.status(400).json({ valid: false, message: 'Failed to create character you dingus' })
+    } else {
+        res.status(201).json({ valid: true, character: newCharacter })
+    }
+})
 
-
-
+//export the router
+export default router
