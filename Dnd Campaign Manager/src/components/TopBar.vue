8b1eef94f1698b31b3abbc0bfcd060617c0af250
MelissaMuckenhirn <muckenhirnm8059@my.uwstout.edu>
2025-10-27 15:30:22 -0500

    Connected to backend and topnav stuff

--- Dnd Campaign Manager/src/components/TopBar.vue
+++ Dnd Campaign Manager/src/components/TopBar.vue
@@ -2,9 +2,18 @@
 
     <div class="topbar">
         <img alt="Mascot" src="../assets/Rat-Squirrel.png" width = "75" height="75"/> 
-        <h1>The DM Domain</h1>
+        <h1>The <div class= DM> DM </div> Domain</h1>
         
         <!-- Profile Picture specific to user -->
         <button class="topbar-button" onclick="window.location.href='/'">Account</button>
     </div>
 </template>
+
+<style scoped>
+.dm {
+    color: var(--vt-c-red);
+    display: inline;
+    padding-left: 10px;
+    padding-right: 10px;
+}
+</style>
