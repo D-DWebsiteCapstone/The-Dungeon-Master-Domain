860a42fa1c596326aaebc9f5be00953f5aa01ec7
Dastas1 <eliasenc3351@my.uwstout.edu>
2025-10-27 16:09:28 -0500

    Did topbar fix

--- Dnd Campaign Manager/src/components/TopBar.vue
+++ Dnd Campaign Manager/src/components/TopBar.vue
@@ -2,7 +2,7 @@
 
     <div class="topbar">
         <img alt="Mascot" src="../assets/Rat-Squirrel.png" width = "75" height="75"/> 
-        <h1>The DM Domain</h1>
+        <h1>The DM Domain hehehe</h1>
         
         <!-- Profile Picture specific to user -->
         <button class="topbar-button" onclick="window.location.href='/'">Account</button>
