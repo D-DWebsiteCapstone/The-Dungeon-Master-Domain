2eecdba8e15206dc774d04da12ea55fe17fb641c
MelissaMuckenhirn <muckenhirnm8059@my.uwstout.edu>
2025-11-13 21:17:50 -0600

    Converting DM in topbar to button

--- Dnd Campaign Manager/src/components/TopBar.vue
+++ Dnd Campaign Manager/src/components/TopBar.vue
@@ -19,7 +19,7 @@ function accountButton(){
         <img  alt="Mascot" src="../assets/Rat-Squirrel.png" width = "55" height="55"/> 
         </button>
         <div class ="center">
-        <h1>The <div class = dm>DM</div> Domain</h1>
+        <h1>The <button class = DMButton>DM</button> Domain</h1>
         </div>
         <button class = "invisibleButton" @click = "accountButton()">Account</button>
     </div>
@@ -32,6 +32,38 @@ function accountButton(){
     display: inline;
     padding-left: 15px;
     padding-right: 15px;
+}
+
+.DMButton{
+    color: var(--vt-c-red);
+    display: inline;
+    padding-left: 10px;
+    padding-right: 0px;
+    padding-bottom: 5px;
+    justify-content: center;
+    align-items: center;
+    font-family: "Eagle Lake", serif;
+    font-weight: 400;
+    font-size: 2rem;
+    background-color: transparent;
+  background-image: none;
+  border: none;
+  cursor: pointer;
+  box-shadow: none;
+  /* min-width:fit-content; */
+  transition: all 0.2s ease-in-out;
+}
+
+.DMButton:hover {
+    color: var(--vt-c-red);
+    display: inline;
+    /* padding-left: 15px;
+    padding-right: 15px; */
+    text-shadow:
+    0 0 6px rgba(160, 48, 34, 0.6),     /*inner*/
+    0 0 12px rgba(212, 175, 55, 0.4),   /*middle*/
+    0 0 20px rgba(212, 175, 55, 0.3);   /*outer*/
+    animation: emberPulse 3s infinite ease-in-out;
 }
 
 .dm:hover {
@@ -52,6 +84,7 @@ button {
     margin-left: 10px;
     margin-right: 20px;
     margin-bottom: 5px;
+    margin-top: 10px;
     padding:0px;
     color: var(--vt-c-warm-white);
 }
