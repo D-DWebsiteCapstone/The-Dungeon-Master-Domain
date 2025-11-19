1d3f1901b49c33892f15babd3ae6fd0ed6f86e2c
pantzlaffw1606 <144815749+pantzlaffw1606@users.noreply.github.com>
2025-11-19 15:40:13 -0600

    Last Calander Stuff (I hope)

--- Dnd Campaign Manager/src/components/Home.vue
+++ Dnd Campaign Manager/src/components/Home.vue
@@ -8,7 +8,7 @@
   
   <!-- Pay Attention -->
   <div class="calendarContainer" >
-    <VDatePicker transparent borderless v-model="selectedDate" :attributes="attributes" />
+    <VCalendar transparent borderless v-model="selectedDate" :attributes="attributes" />
   </div>
     <!-- <VDatePicker v-model="date" mode="dateTime" hide-time-header :attributes="attributes" /> -->
   <!-- Pay Attention -->
@@ -159,13 +159,13 @@ async function CampaignSort() {
 
 const attributes = ref([
   {
-    highlight: 'blue',
+    highlight: 'red',
     dates: [  
       new Date(),
     ],
 },
   {
-    highlight: 'green',
+    highlight: 'blue',
     dates: [
       new Date(2025, 9, 28),
       new Date(2025, 10, 2),
