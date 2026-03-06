15c7f44c809a1deafa7cd4d6fff191ff7783170a
herzi02 <146674438+herzi02@users.noreply.github.com>
2026-03-06 15:17:20 +0000

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+//Commented out to allow functionality for people using node versions above 22.12.0
+//Wasn't even being used on our website anyways
+//import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "unsafe-none"
+    }
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
