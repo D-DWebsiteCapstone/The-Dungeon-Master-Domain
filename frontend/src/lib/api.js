79471443d01250cee733c5b0c3ab7439478d809a
Connor Larson <larsonconnor2004@gmail.com>
2026-04-02 18:39:02 -0500

    I believe I fixed the backend url call

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,9 @@
 // Determine if we are running locally
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-const API_BASE = isLocal
-  ? "http://localhost:3000"
-  : import.meta.env.VITE_BACKEND_URL;
-
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+  ? `${import.meta.env.VITE_BACKEND_URL}/api`
+  : 'http://localhost:3000/api'
 // Build full request URL
 export function apiUrl(path = "") {
   if (!path.startsWith("/")) {
