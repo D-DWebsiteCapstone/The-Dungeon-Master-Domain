60d6e2a37c83adfcc6506e6cb267630a6f39636e
Connor Larson <larsonconnor2004@gmail.com>
2026-04-02 18:47:37 -0500

    variable name adjustment

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 // Determine if we are running locally
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL
+const API_BASE = import.meta.env.VITE_BACKEND_URL
   ? `${import.meta.env.VITE_BACKEND_URL}/api`
   : 'http://localhost:3000/api'
 // Build full request URL
