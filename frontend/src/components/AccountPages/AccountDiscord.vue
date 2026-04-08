c28f7b7623c2c426db69132d8665399e6b8c9755
MelissaMuckenhirn <muckenhirnm8059@my.uwstout.edu>
2026-04-08 11:50:14 -0500

    Basic formatting on discord account page for account linking

--- frontend/src/components/AccountPages/AccountDiscord.vue
+++ frontend/src/components/AccountPages/AccountDiscord.vue
@@ -9,10 +9,31 @@
         </div>
 
         <p>Link your Discord account to your profile.</p>
+
+        <div class="ifAccount">
+            <div class="noAccount" v-if="!discordUsername">
+              <div class="currentTitle"><p>Current linked account: </p></div>
+              <div class="accountName"><p>No account linked</p></div>
+              <div class="accountOption">
+                <input type="text" placeholder="Enter Discord Username">
+                <button class="popupButton">Link Account</button>
+              </div>
+            </div>
+            <div class="linkedAccount" v-else>
+              <div class="currentTitle"><p>Current linked account: </p></div>
+              <div class="accountName"><p>{{ discordUsername }}</p></div>
+              <div class="accountOption"><button class="parchmentButton">Unlink Account</button></div>
+            </div>
+        </div>
   
     </div>
 
 </template>
+
+<script setup>
+const discordUsername = null; // Placeholder for the actual Discord username
+
+</script>
 
 <style scoped>
 .parchmentButton {
@@ -24,7 +45,6 @@
   margin: 0;
   align-items: top;
 }
-
 
 .dividerh2 {
   display: inline;
@@ -44,5 +64,59 @@ img {
   max-width: 28%;
   margin-left: 10px;
   margin-right: 10px;
+}
+
+.ifAccount {
+  align-items: center;
+  justify-content: center;
+  margin-left: 1rem;
+  margin-top: 1rem;
+  width: 90%;
+  border: 1px solid var(--vt-c-bronze);
+  border-radius: 8px;
+  padding: 1.5rem;
+  backdrop-filter: blur(2px);
+}
+
+.noAccount, .linkedAccount {
+  display: grid;
+  grid-template-columns: 1fr 1.25fr;
+  grid-template-rows: 0.5fr 1fr;
+}
+
+.currentTitle {
+  grid-column: 1;
+  grid-row: 1;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  padding-right: 3%;
+}
+
+.accountName {
+  grid-column: 2;
+  grid-row: 1;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  
+  p {
+    font-size: 1.2rem;
+  }
+}
+
+.accountOption {
+  grid-row: 2;
+  grid-column: 1/span 2;
+  display: flex;
+  justify-content: space-between;
+
+  input {
+    width: 65%;
+  }
+
+  .popupButton {
+    border: 1px solid var(--vt-c-bronze);
+  }
 }
 </style>
