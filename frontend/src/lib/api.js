f4683cce144f9a17d74a09b144600e184e0445d9
Connor Larson <larsonconnor2004@gmail.com>
2026-04-08 12:53:17 -0500

    temp fix for allowed origins

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,8 +2,8 @@
 const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
 const API_BASE = import.meta.env.VITE_BACKEND_URL
-  ? `${import.meta.env.VITE_BACKEND_URL}`
-  : 'http://localhost:3000'
+  ? 'http://localhost:3000'
+  : `${import.meta.env.VITE_BACKEND_URL}`
 // Build full request URL
 
 export function apiUrl(path = "") {
